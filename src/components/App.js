6585b9c36f301f11e9377b47bc321e25764206a1
Oleg Lytvyn <igocooper@gmail.com>
2018-04-15 21:09:18 +0300

    authorized request using Apollo-link middleware

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
-import Header from './Header'
-import { Switch, Route } from 'react-router-dom'
+import Header from './Header';
+import Login from './Login';
+
 
 class App extends Component {
   render() {
@@ -11,6 +14,7 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
+            <Route exact path="/login" component={Login} />
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
           </Switch>
