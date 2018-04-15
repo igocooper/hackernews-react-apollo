09c76a491f19e5bbd528cfab8e59ed95bb08c065
Oleg Lytvyn <igocooper@gmail.com>
2018-04-15 18:11:33 +0300

    server started

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-    <ApolloProvider>
+    <ApolloProvider client={client}>
         <App />
     </ApolloProvider>, 
     document.getElementById('root')
