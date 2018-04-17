1aa551c222049ef36855cf72a413d8ac5b2ceb73
Oleg Lytvyn <igocooper@gmail.com>
2018-04-18 00:21:12 +0300

    pagination

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,7 +2,8 @@ import React, { Component, Fragment } from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 // components import
-import Link from './Link'
+import Link from './Link';
+import { LINKS_PER_PAGE } from '../constants'
 
 class LinkList extends Component {
     componentDidMount() {
@@ -11,16 +12,17 @@ class LinkList extends Component {
       }
       
     _updateCacheAfterVote = (store, createVote, linkId) => {
-        // 1
-        const data = store.readQuery({ query: FEED_QUERY })
-      
-        // 2
+        const isNewPage = this.props.location.pathname.includes('new')
+        const page = parseInt(this.props.match.params.page, 10)
+        const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+        const first = isNewPage ? LINKS_PER_PAGE : 100
+        const orderBy = isNewPage ? 'createdAt_DESC' : null
+        const data = store.readQuery({ query: FEED_QUERY, variables: { first, skip, orderBy } })
+        
         const votedLink = data.feed.links.find(link => link.id === linkId)
         votedLink.votes = createVote.link.votes
-      
-        // 3
         store.writeQuery({ query: FEED_QUERY, data })
-      };
+    };
 
     _subscribeToNewLinks = () => {
     this.props.feedQuery.subscribeToMore({
@@ -92,62 +94,107 @@ class LinkList extends Component {
     })
     };
 
-    render() {
-    const { feedQuery } = this.props;
-
-    // wait for data to load
-    if (feedQuery && feedQuery.loading) {
-        return <div>Loading</div>
-    }
-
-    // handle Error
-    if (feedQuery && feedQuery.error) {
-        return <div>Error</div>
-    }
-
-    const linksToRender = this.props.feedQuery.feed.links
-
-    return (
-    <Fragment>
-        {linksToRender.map((link, index) => {
-            return <Link 
-                key={link.id} 
-                index={index} 
-                link={link} 
-                updateStoreAfterVote={this._updateCacheAfterVote}
-            />
-        })
+    _getLinksToRender = (isNewPage) => {
+        if (isNewPage) {
+          return this.props.feedQuery.feed.links
         }
-    </Fragment>
-    )
+        const rankedLinks = this.props.feedQuery.feed.links.slice()
+        rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length)
+        return rankedLinks
+      }
+
+    _nextPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page <= this.props.feedQuery.feed.count / LINKS_PER_PAGE) {
+        const nextPage = page + 1
+        this.props.history.push(`/new/${nextPage}`)
+    }
+    }
+    
+    _previousPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page > 1) {
+        const previousPage = page - 1
+        this.props.history.push(`/new/${previousPage}`)
+    }
+    }
+
+
+    render() {
+        const { feedQuery } = this.props;
+
+        // wait for data to load
+        if (feedQuery && feedQuery.loading) {
+            return <div>Loading</div>
+        }
+
+        // handle Error
+        if (feedQuery && feedQuery.error) {
+            return <div>Error</div>
+        }
+
+        const isNewPage = this.props.location.pathname.includes('new');
+        const linksToRender = this._getLinksToRender(isNewPage);
+        const page = parseInt(this.props.match.params.page, 10);
+
+        return (
+        <Fragment>
+            {linksToRender.map((link, index) => {
+                return <Link 
+                    key={link.id} 
+                    index={index} 
+                    link={link} 
+                    updateStoreAfterVote={this._updateCacheAfterVote}
+                />
+            })
+            }
+            {isNewPage &&
+            <div className='flex ml4 mv3 gray'>
+                <div className='pointer mr2' onClick={() => this._previousPage()}>Previous</div>
+                <div className='pointer' onClick={() => this._nextPage()}>Next</div>
+            </div>
+            }
+        </Fragment>
+        )
     }
 }
 
 
 export const FEED_QUERY = gql`
-    query FeedQuery {
-        feed {
-            links {
-                id 
-                createdAt
-                url
-                description
-                postedBy {
-                    id
-                    name
-                }
-                votes {
-                    id
-                    user {
-                        id
-                    }
-                }
-
-            }
+query FeedQuery($first: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+  feed(first: $first, skip: $skip, orderBy: $orderBy) {
+    count
+    links {
+      id
+      createdAt
+      url
+      description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
         }
+      }
     }
-`;
+    count
+  }
+}
+`
 
-export default graphql(FEED_QUERY,{
-    name: 'feedQuery'
-})(LinkList);
+export default graphql(FEED_QUERY, {
+    name: 'feedQuery',
+    options: ownProps => {
+      const page = parseInt(ownProps.match.params.page, 10)
+      const isNewPage = ownProps.location.pathname.includes('new')
+      const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+      const first = isNewPage ? LINKS_PER_PAGE : 100
+      const orderBy = isNewPage ? 'createdAt_DESC' : null
+      return {
+        variables: { first, skip, orderBy },
+      }
+    },
+  })(LinkList)
